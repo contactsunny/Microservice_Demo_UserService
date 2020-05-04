ef76e04d7e9a0f163db3e58dda40200d39a10435
Sunny Sriniidhi <sunny.3mysore@gmail.com>
2020-05-04 14:30:05 +0530

    Added Eureka client and discovery.

--- src/main/java/com/contactsunny/poc/user_microservice_example/services/AddressService.java
+++ src/main/java/com/contactsunny/poc/user_microservice_example/services/AddressService.java
@@ -3,6 +3,7 @@ package com.contactsunny.poc.user_microservice_example.services;
 import com.contactsunny.poc.user_microservice_example.dtos.Address;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -17,9 +18,11 @@ public class AddressService {
     @Value("${address.api.baseUrl}")
     private String addressServiceBaseUrl;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     public List<Address> getAddressForUser(int userId) {
-        String url = addressServiceBaseUrl + "/address/" + userId;
-        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://address-service/address/" + userId;
         String response = restTemplate.getForObject(url, String.class);
         Type addressListType = new TypeToken<ArrayList<Address>>(){}.getType();
         List<Address> addressList = new Gson().fromJson(response, addressListType);
--- src/main/java/com/contactsunny/poc/user_microservice_example/services/UserService.java
+++ src/main/java/com/contactsunny/poc/user_microservice_example/services/UserService.java
@@ -1,7 +1,6 @@
 package com.contactsunny.poc.user_microservice_example.services;
 
 import com.contactsunny.poc.user_microservice_example.dtos.Address;
-import com.contactsunny.poc.user_microservice_example.dtos.AddressList;
 import com.contactsunny.poc.user_microservice_example.dtos.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
